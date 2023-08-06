06033584228b63350fa17cd41377d4cfb4af9940
tylerroliverr <114887429+tylerroliverr@users.noreply.github.com>
2023-08-06 16:28:04 +1000

    added mega nav

--- app.js
+++ app.js
@@ -1,3 +1,56 @@
+// const customCursor = document.getElementById('custom-cursor');
+// const hoverContainer = document.querySelector('.hover-container');
+
+// const updateCursorPosition = (event) => {
+//    customCursor.style.top = `${event.clientY}px`;
+//    customCursor.style.left = `${event.clientX}px`;
+// };
+
+// window.addEventListener('mousemove', (event) => {
+//    updateCursorPosition(event)
+//    if (hoverContainer.matches(':hover')) {
+//       customCursor.classList.add('zoom')
+//    } else {
+//       customCursor.classList.remove('zoom')
+//    }
+// });
+
+const menuIcon = document.querySelector(".menu-text");
+const closeIcon = document.querySelector(".close-text");
+const megaNav = document.querySelector(".mega-nav");
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenuLinks));
+function closeMenuLinks() {
+   megaNav.classList.remove("active");
+   closeIcon.style.display = "none";
+   menuIcon.style.display = "block";
+}
+
+// SHOW MEGA MENU (SHOW CLOSE ICON AND HIDE MENU ICON)
+const openMenu = () => {
+   if (megaNav.classList.contains("active")) {
+      megaNav.classList.remove("active");
+      console.log("yup")
+   } else {
+      megaNav.classList.add("active");
+      console.log("hmmm")
+      menuIcon.style.display = "none";
+      closeIcon.style.display = "block";
+   }
+};
+
+// HIDE MEGA MENU (HIDE CLOSE ICON AND SHOW MENU ICON)
+const closeMenu = () => {
+   megaNav.classList.remove("active");
+   console.log("diekk");
+   closeIcon.style.display = "none";
+   menuIcon.style.display = "block";
+};
+
+menuIcon.addEventListener("click", openMenu);
+closeIcon.addEventListener("click", closeMenu);
+
 var myIndex = 0;
 var myIndex2 = 0;
 carouselImages();
@@ -26,3 +79,10 @@ function carouselText() {
    x2[myIndex2 - 1].style.display = "block";
    setTimeout(carouselText, 1300); //1000 = 1 second
 }
+
+`use strict`;
+function refreshTime() {
+   var datetime = new Date().toLocaleTimeString();
+   document.getElementById("time").textContent = datetime;
+}
+   setInterval(refreshTime, 1000);
