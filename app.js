b050a157a678eeeff7c9dd5e39d401d22d772544
tylerroliverr <114887429+tylerroliverr@users.noreply.github.com>
2023-08-07 19:24:32 +1000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+// let timer;
+// let timerLoad;
+
+// function showPage() {
+//    document.getElementById('loader').style.display = 'none';
+//    document.getElementById('before-loading').style.display = 'block';
+// }
+
+// function pageLoaded () {
+//    document.getElementById('loader').classList.add('loaded');
+// }
+
+// function loadingStar() {
+//    timerLoad = setTimeout(pageLoaded, 3000);
+//    timer = setTimeout(showPage, 3500);
+// }
+
+window.addEventListener("scroll", function() {
+   const logo = document.querySelector('.logo');
+   const scrollValue = window.scrollY;
+
+   // Calculate the blur amount based on the scroll position
+   const maxBlur = 1000; // Maximum blur amount in pixels
+   const blurAmount = Math.min(scrollValue / 100, maxBlur); // Adjust divisor for sensitivity
+
+   // Apply the blur effect
+   logo.style.filter = `blur(${blurAmount}px)`;
+});
